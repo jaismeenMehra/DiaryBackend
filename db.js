3726b4306f5faeb1b184244db54fe13e8a769f4d
Jaismeen <jaismeenmehra9@gmail.com>
2023-08-22 10:50:54 +0530

    connected to database

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/Diary";
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("connected to mongo successfully")
+    })
+}
+
+
+module.exports = connectToMongo;
