0b5e10c910a33e5d078a2621c4431cb36a7d6a80
Jaismeen <jaismeenmehra9@gmail.com>
2023-09-05 11:17:23 +0530

    added endpoint to fetch and add notes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // Avalibale Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('api/notes',require('./routes/notes'));
+app.use('/api/notes',require('./routes/notes'));
 
 
 
--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const Notes = require('../models/Notes');
+const fetchuser = require('../middleware/fetchuser');
+const {body, validationResult} = require('express-validator');
 
-router.get('/',(req,res)=>{
+// ROUTE 1 : Get all notes using: GET "/api/notes/fetchallnotes". Login required
+router.get('/fetchallnotes',fetchuser,async (req,res)=>{
 
-    obj={
-        a:'this',
-        number:34
-    };
+try{    // as middleware is used req.user will have user 
+   const notes = await Notes.find({user:req.user.id})
+    res.json(notes);
+}
+catch(error){
+    console.error(error.message);
+    res.status(500).send("It's not you , It's us \n Server Error.");
+}
+});
 
-    res.json(obj);
-    
 
+// ROUTE 2 : Add a new note using: POST "/api/notes/addnote". Login required
+router.post('/addnote',fetchuser,[
+    body('title','Enter a valid string').isLength({min: 4}),
+    body('description','Enter a valid description').isLength({min:10})
+],async (req,res)=>{
+    try{
+
+        // checking for validation errors
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+        const {title, description, tag} = req.body;
+        const newNote = new Notes({
+            title, description, tag, user: req.user.id
+        })
+        
+        const savedNote = await newNote.save();
+        res.json(newNote);
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("It's not you , It's us \n Server Error.");
+    }
+   
 });
 
 
